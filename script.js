b71ba66b8c0bfce88e7fe7868138f716319dfdfd
YukselYslcmn <yuksel6491@hotmail.com>
2023-10-24 00:08:14 +0300

    update

---
+++ script.js
@@ -0,0 +1,11 @@
+<form action="" method="post">
+
+  <label for="kadi">Kullanıcı adı</label>
+  <input type="text" name="kullanici_adi" id="kadi"> <br>
+
+    <label for="sifre">Şifre</label>
+    <input type="password" name="sifre" id="sifre"> <br>
+
+      <button type="submit">Giriş yap</button>
+
+    </form>
